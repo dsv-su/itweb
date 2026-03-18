06f567158e6b84ff4e4f09793bbfb766d0252730
orkit <ryan@dias.se>
2026-03-18 10:08:23 +0100

    Moved mobile darkmode switch to navbar

--- resources/views/dsvheader.blade.php
+++ resources/views/dsvheader.blade.php
@@ -11,6 +11,7 @@
         </div>
         <div class="items-center inline-flex justify-start col-span-3 sm:flex px-5 bg-sudepartment">
             <span class="self-center text-base font-normal font-sudepartment whitespace-pre-line text-white dark:text-white">{{__("Department of Computer and Systems Sciences")}}</span>
+            {{--}}
             <!-- Dark mode switcher (mobile) -->
             <button
                 type="button"
@@ -37,7 +38,7 @@
                     <path d="M10 15a5 5 0 1 0 0-10 5 5 0 0 0 0 10Zm0-11a1 1 0 0 0 1-1V1a1 1 0 0 0-2 0v2a1 1 0 0 0 1 1Zm0 12a1 1 0 0 0-1 1v2a1 1 0 1 0 2 0v-2a1 1 0 0 0-1-1ZM4.343 5.757a1 1 0 0 0 1.414-1.414L4.343 2.929a1 1 0 0 0-1.414 1.414l1.414 1.414Zm11.314 8.486a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM4 10a1 1 0 0 0-1-1H1a1 1 0 0 0 0 2h2a1 1 0 0 0 1-1Zm15-1h-2a1 1 0 1 0 0 2h2a1 1 0 0 0 0-2ZM4.343 14.243l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414a1 1 0 0 0-1.414-1.414ZM14.95 6.05a1 1 0 0 0 .707-.293l1.414-1.414a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 .707 1.707Z"></path>
                 </svg>
                 <span class="sr-only">Toggle dark/light mode</span>
-            </button>
+            </button>{{--}}
         </div>
     </div>
 
